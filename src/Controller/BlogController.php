9f8ad3084870d0cc6849e8d3396947a03c65a0a2
dds21v <ddsvideastee@gmail.com>
2019-04-02 10:30:23 +0200

    Day Two: Twig & Annotations

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,18 +4,19 @@
 namespace App\Controller;
 
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AboutController extends AbstractController
+class BlogController extends AbstractController
 {
     /**
-     * @Route("/about",name="app_about")
+     * @Route("/page/{id}/", name="app_blog")
      * @return Response
      */
-    public function about(): Response
+    public function page(): Response
     {
-        return $this->render('about.html.twig');
+        return $this->render('blog.html.twig');
     }
 }
