bd27b3c4518c491d18490b6655180f038c45c4d7
dds21v <ddsvideastee@gmail.com>
2019-04-02 13:44:22 +0200

    Day two: Api/Json

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,22 +1,51 @@
 <?php
-
-
 namespace App\Controller;
 
-
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/page/{id}/", name="app_blog")
+     * @Route("/page/{id}", requirements={"id"="[0-9]+"}, name="app_blog")
+     * @param int|null $id
      * @return Response
      */
-    public function page(): Response
+    public function page(?int $id = null): Response
+    {
+        var_dump($id);
+        return $this->render('blog.html.twig');
+    }
+
+    public function page2(?string $id = null): Response
     {
         return $this->render('blog.html.twig');
+    }
+
+    /**
+     * @Route("/page3/{id<[0-9]+>}", methods={"GET"}, name="app_blog_3")
+     * @param int|null $id
+     * @return Response
+     */
+    public function page3(?int $id = null): Response
+    {
+        return $this->render('blog.html.twig');
+    }
+
+    /**
+     * @Route("/api/product")
+     * @return JsonResponse
+     */
+    public function displayJson(): JsonResponse
+    {
+        $product = [
+            "id" => 1,
+            "name" => 'hamac',
+            "price" => 10.55
+        ];
+
+        return $this->json($product);
     }
 }
